c656cdb78065b2e6f3c1e100bdb6590104be2a37
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-13 15:39:27 -0300

    First commit

---
+++ pingpongserver.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	service := ":1201"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+		stringReceived := strings.TrimSpace(string(buf[0:n]))
+		if strings.Contains(stringReceived, "ping") {
+		  _, err = conn.Write([]byte("pong"))
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
